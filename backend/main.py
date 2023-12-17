da2e8a9108bee40f9a5d51dde2f5fae2844f0b75
Holland Pleskac <hollandpleskac@gmail.com>
2023-12-16 22:20:25 -0800

    server integration

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,27 @@ from typing import Union
 
 app = FastAPI()
 
-# get prediction
+def SKLinReg(data):
+
+  # Split the data into training and testing sets
+  features= ['Open','Volume','High','Low']
+  target = ['Close']
+  traindata = data.iloc[:int(.90*len(data)),:]
+  test = data.iloc[int(.90*len(data)):,:]
+
+  # Create a linear regression model
+  model = LinearRegression()
+
+  # Fit the model to the training data
+  model.fit(traindata[features], traindata['Close'])
+
+  # Evaluate the model on the testing data
+  score = model.score(test[features],  test['Close'])
+  print('this model score is(sklearn) ',score)
+  # Make predictions using the model
+  pred = model.predict(test[features])
+  return pred , score
+
 def xgbpriceclose(data):
   features= ['Open','Volume','High','Low']
   traindata = data.iloc[:int(.90*len(data)),:]
@@ -16,17 +36,51 @@ def xgbpriceclose(data):
   model = ai.XGBRegressor()
   model.fit(traindata[features],traindata['Close'])
   predictions = model.predict(test[features])
-  print(predictions)
   a=model.score(test[features],test['Close'])
-  print('this model score is(XGB) ',a)
+
   return predictions, a
 
-#data grab
+
+def XgbSupLearn(data, days):
+  #make model and set data so previous close price is input to find the next one
+  model = ai.XGBRegressor()
+  inputdata = data['Close'].shift(1)
+  targetdata = data['Close']
+  model.fit(inputdata, targetdata)
+  pred = model.predict(inputdata.values[int(.90*len(data)):])
+  future = list()
+  score=model.score(inputdata,targetdata)
+
+  for n in range(days):
+      inputdata = data['Close'].shift(1)
+      nextday = model.predict(inputdata[-1:])
+      newday = inputdata.index[-1] + pd.Timedelta(days=1)
+      newp = pd.DataFrame({'Date': [newday], 'Close': nextday})
+      newp.set_index('Date', inplace=True)
+      data = pd.concat([data,newp])
+      future.append(newp)
+
+  return pred,score,future
+  #data grab
 def stockdata(ticker,period):
   tickers = yf.Ticker(ticker)
   data = tickers.history(period=period)
   return data
 
+def aimodels(ticker,period,days):
+  data = stockdata(ticker,period)
+  XGBgraph ,a = xgbpriceclose(data)
+  SKLGraph, ska = SKLinReg(data)
+  XGBSUPGraph ,score,pred = XgbSupLearn(data,days)
+  scores = [a,ska,score]
+  XGBgraph =XGBgraph.tolist()
+  SKLGraph=SKLGraph.tolist()
+  XGBSUPGraph.tolist()
+  
+  # Convert pred to a list of floats
+  pred_floats = [df['Close'].values[0] for df in pred]
+
+  return XGBgraph, SKLGraph, XGBSUPGraph, scores, pred_floats
 
 @app.get("/")
 def read_root():
@@ -37,14 +91,11 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/predict/{ticker}/{period}")
-async def predict_stock(ticker: str, period: str):
-    ticker = 'mcd'
-    period = '1y'
-    data = stockdata(ticker,period)
+@app.get("/predict/{ticker}/{period}/{days}")
+async def predict_stock(ticker: str, period: str, days: int):
+    _, _, _, _, pred = aimodels(ticker, period, days)
 
-    XgbP ,a =xgbpriceclose(data)
+    # Convert numpy.float32 to Python native float
+    pred = [float(p) for p in pred]
 
-    lastest_prediction = float(list(XgbP)[-1])
-
-    return {"ticker": ticker, "period": period, "prediction": lastest_prediction}
+    return {"prediction": pred}
--- backend/test.py
+++ backend/test.py
@@ -5,7 +5,26 @@ import xgboost as ai
 import pandas as pd
 from typing import Union
 
+def SKLinReg(data):
 
+  # Split the data into training and testing sets
+  features= ['Open','Volume','High','Low']
+  target = ['Close']
+  traindata = data.iloc[:int(.90*len(data)),:]
+  test = data.iloc[int(.90*len(data)):,:]
+
+  # Create a linear regression model
+  model = LinearRegression()
+
+  # Fit the model to the training data
+  model.fit(traindata[features], traindata['Close'])
+
+  # Evaluate the model on the testing data
+  score = model.score(test[features],  test['Close'])
+  print('this model score is(sklearn) ',score)
+  # Make predictions using the model
+  pred = model.predict(test[features])
+  return pred , score
 
 def xgbpriceclose(data):
   features= ['Open','Volume','High','Low']
@@ -15,28 +34,32 @@ def xgbpriceclose(data):
   model = ai.XGBRegressor()
   model.fit(traindata[features],traindata['Close'])
   predictions = model.predict(test[features])
-  print(predictions)
   a=model.score(test[features],test['Close'])
-  print('this model score is(XGB) ',a)
-  return predictions[-1]
 
-
-def xgbpriceclose(data):
-  features= ['Open','Volume','High','Low']
-  traindata = data.iloc[:int(.90*len(data)),:]
-  #data to test the model
-  test = data.iloc[int(.90*len(data)):,:]
-  model = ai.XGBRegressor()
-  model.fit(traindata[features],traindata['Close'])
-  predictions = model.predict(test[features])
-  print(predictions)
-  a=model.score(test[features],test['Close'])
-  print('this model score is(XGB) ',a)
   return predictions, a
 
 
+def XgbSupLearn(data, days):
+  #make model and set data so previous close price is input to find the next one
+  model = ai.XGBRegressor()
+  inputdata = data['Close'].shift(1)
+  targetdata = data['Close']
+  model.fit(inputdata, targetdata)
+  pred = model.predict(inputdata.values[int(.90*len(data)):])
+  future = list()
+  score=model.score(inputdata,targetdata)
 
-#data grab
+  for n in range(days):
+      inputdata = data['Close'].shift(1)
+      nextday = model.predict(inputdata[-1:])
+      newday = inputdata.index[-1] + pd.Timedelta(days=1)
+      newp = pd.DataFrame({'Date': [newday], 'Close': nextday})
+      newp.set_index('Date', inplace=True)
+      data = pd.concat([data,newp])
+      future.append(newp)
+
+  return pred,score,future
+  #data grab
 def stockdata(ticker,period):
   tickers = yf.Ticker(ticker)
   data = tickers.history(period=period)
@@ -44,8 +67,22 @@ def stockdata(ticker,period):
 
 ticker = 'mcd'
 period = '1y'
-data = stockdata(ticker,period)
-print(data)
+days = 6
+def aimodels(ticker,period,days):
+  data = stockdata(ticker,period)
+  XGBgraph ,a = xgbpriceclose(data)
+  SKLGraph, ska = SKLinReg(data)
+  XGBSUPGraph ,score,pred = XgbSupLearn(data,days)
+  scores = [a,ska,score]
+  XGBgraph =XGBgraph.tolist()
+  SKLGraph=SKLGraph.tolist()
+  XGBSUPGraph.tolist()
+  
+  # Convert pred to a list of floats
+  pred_floats = [df['Close'].values[0] for df in pred]
 
-XgbP ,a =xgbpriceclose(data)
-print(XgbP)
+  return XGBgraph, SKLGraph, XGBSUPGraph, scores, pred_floats
+
+
+g,g,g,s,pred = aimodels(ticker,period,days)
+print(pred)
